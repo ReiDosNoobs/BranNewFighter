1ab8f114c0d20228e5b0e70da088d6c1b015bba6
ReiDosNoobs <leonardo48rodrigues@gmail.com>
2023-11-29 23:03:43 -0300

    Inimigo_funcionando
    O inimigo esta descendo e subindo sentando pro mbappe

--- Assets/Scripts/Boss_Walk.cs
+++ Assets/Scripts/Boss_Walk.cs
@@ -6,7 +6,8 @@ public class Boss_Walk : StateMachineBehaviour
 {
     public float speed = 2.5f;
     public float attackRange = 1.15f;
-    //public int countAttack = 0;
+    //private float spawnRate = 6f;
+    public int countAttack = 0;
     //public float time_rate = 2f;
     //float next_time = 0f;
 
@@ -26,33 +27,50 @@ public class Boss_Walk : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         boss.LookAtPlayer();
-        
+
         Vector2 target = new Vector2(player.position.x, player.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         Vector2 opPos = Vector2.MoveTowards(rb.position, target, -speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
-        /*if (countAttack >= 2)
-        {
-            Debug.Log("dois ataque");
-            rb.MovePosition(opPos);
-            next_time = Time.time + 1f / time_rate;
-            if(next_time >= 5)
-            {
-                countAttack = 0;
-            }
 
-        }*/
+        if (countAttack >= 1)
+        {
+            rb.MovePosition(opPos);
+            
+            
+            if (Vector2.Distance(player.position, rb.position) >= 7)
+             {
+                 countAttack = 0;
+             }
+        }
 
         if (Vector2.Distance(player.position, rb.position) <= attackRange)
         {
             animator.SetTrigger("Attack");
-            //countAttack = countAttack + 1;
         }
     }
+
+    /*private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
+
+        Debug.Log("dois ataque");
+        rb.MovePosition(opPos);
+
+        if (wait >= 5)
+        {
+            countAttack = 0;
+        }
+    }*/
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("Attack");
+        countAttack = countAttack + 1;
+        /*Vector2 target = new Vector2(player.position.x, player.position.y);
+        Vector2 opPos = Vector2.MoveTowards(rb.position, target, -speed * Time.fixedDeltaTime);
+        rb.MovePosition(opPos);*/
     }
 }
